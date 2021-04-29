d8cafccdf45195eebdb720138e43c572711aaa4b
taishi.develop@gmail.com <taishi.develop@gmail.com>
2021-04-30 01:32:38 +0900

    add type alias

--- increment/increment.js
+++ increment/increment.js
@@ -2,3 +2,8 @@ function increment(num) {
     return num + 1;
 }
 console.log(increment(1));
+var showMessages = function (msgs) {
+    msgs.forEach(function (msg) { return console.log(msg); });
+};
+var forYou = ["Hi", "This message", "is", "for you."];
+showMessages(forYou);
--- increment/increment.ts
+++ increment/increment.ts
@@ -1,5 +1,15 @@
 function increment(num: number): number{
-    return num+1;
+    return num+1
 }
 
-console.log(increment(1));
+console.log(increment(1))
+
+// type alias
+type Messages = string[]
+
+const showMessages = (msgs: Messages) => {
+    msgs.forEach(msg => console.log(msg))
+}
+
+const forYou: Messages = ["Hi","This message","is","for you."]
+showMessages(forYou)
